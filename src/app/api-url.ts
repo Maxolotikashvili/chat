736ea1ad3fcbe0d5b0b5310ce3070890a2702dd3
Maxolotikashvili <88730788+Maxolotikashvili@users.noreply.github.com>
2023-08-05 16:34:34 +0400

    update 1.7

--- src/app/api-url.ts
+++ src/app/api-url.ts
@@ -1,1 +1,2 @@
-export const API_URL: string = 'https://chat-api-production-8779.up.railway.app/';
+const serverApi: string = 'https://chat-api-production-8779.up.railway.app/';
+export const API_URL: string = serverApi;
